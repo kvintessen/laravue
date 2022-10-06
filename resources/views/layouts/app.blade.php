ee6b21565f1c26402f26c7339c9b4da74bafd098
eegorov <eegorov@kazanworkout.ru>
2022-10-07 02:43:12 +0300

    SD-2. Изменена ссылка на GitHub

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
                        href="{{ route('article.index')  }}">Каталог статей</a>
                 </li>
             </ul>
-            <a class="d-flex justify-content-end " href="https://github.com/egorkgeu">
+            <a class="d-flex justify-content-end " href="https://github.com/kvintessen">
                 <i class="bi bi-github" style="font-size: 2rem; color: #000000;"></i>
             </a>
         </div>
